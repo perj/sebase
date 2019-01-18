76b9a899bd564e10637da585d70b117be4ea6f01
Per Johansson <per@morth.org>
2019-01-18 14:37:31 +0100

    Disable the go init functions on OS X.
    For some reason the weak symbol imports generate undefined symbol
    errors. Possibly because the symbols do in fact exist but are private,
    not sure. Regardless sebuild can't disable the automatic Go runtime
    initialization on OS X either, so just disabling these functions should
    work fine for now (just don't fork on OS X).

--- util/lib/goinit.c
+++ util/lib/goinit.c
@@ -4,6 +4,22 @@
 #include "macros.h"
 #include "linker_set.h"
 
+#ifdef __APPLE__
+// For some reason the weak import don't work properly on OS X.
+// I won't investigate this further right now as we also can't disable the
+// auto-load of the go environment on OS X right now.
+// As we don't know if the Go runtime exists false seems like the safe choice.
+
+bool
+has_go_runtime(void) {
+	return false;
+}
+
+bool
+init_go_runtime(int argc, char *const *argv) {
+	return false;
+}
+#else
 extern void GO_INIT_LIB_SYMBOL(int argc, char *const*argv, char **environ) WEAK;
 extern void _cgo_wait_runtime_init_done(void) WEAK;
 extern char **environ;
@@ -21,3 +37,4 @@ init_go_runtime(int argc, char *const *argv) {
 	_cgo_wait_runtime_init_done();
 	return true;
 }
+#endif
