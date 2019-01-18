9cf86aaa9066e53184c4c475d246be67cbfd3337
Per Johansson <per@morth.org>
2019-01-18 09:43:47 +0100

    Use DYLD variables to load sysloghook on OS X.
    This only works for this one test where the sysloghook is required, but
    I don't think there's much I can do about it. Using any binary from /bin
    or /usr/bin while system protection is enabled disables these variables
    from that point on.
    
    Regress-runner still only sets LD_PRELOAD. Obviously that has no effect
    on OS X except to be forwarded to this makefile.

--- util/test/xerr/regress-runner.mk
+++ util/test/xerr/regress-runner.mk
@@ -12,7 +12,14 @@ test_err:
 test_errno:
 	test "`sh -c "test_xerr -n 2>&1"`" = "test_xerr: hej: Invalid argument"
 
+# On OS X, all system binaries (e.g. make and sh) unset all DYLD environment
+# variables for security. Thus we need to set them here just before running our
+# custom binary when testing sysloghook.
 test_sl_errno:
 	echo INVALIDATE PREVIOUS TESTRUN >> ${SYSLOGROOT}/xerr.log
-	${SYSLOGHOOK} test_xerr -ns
+	if [ "$$(uname)" = Darwin ]; then \
+		export DYLD_INSERT_LIBRARIES=$$LD_PRELOAD; \
+		export DYLD_FORCE_FLAT_NAMESPACE=1; \
+	fi; \
+	test_xerr -ns
 	tail -1 ${SYSLOGROOT}/xerr.log | grep -q "xerr: hej: Invalid argument"
